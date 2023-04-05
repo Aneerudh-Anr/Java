8fb39834b4b8f785cd2a3a709d74ffedbb3eef32
Aneerudh-Anr <aneerudhb@gmail.com>
2023-04-05 23:06:27 +0530

    variables

--- Programs/variable.java
+++ Programs/variable.java
@@ -4,5 +4,7 @@ public static void main(String args[])
 	{
 		System.out.print(4+5); //this will also works but there will be no new line so output is like 98
         System.out.println(3+5);
+        int num = 3;
+        System.out.println(num);
 	}
 }
